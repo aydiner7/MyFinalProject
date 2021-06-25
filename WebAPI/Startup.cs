6eba2cc87b9b9edff86c7baadddd214768f88f77
hasanaydiner <hasanaydiner7@hotmail.com>
2021-06-25 03:44:12 +0300

    Proje dosyası ekle.

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -31,8 +31,8 @@ namespace WebAPI
         {
             services.AddControllers();
             // ProductService istenirse, ona productManager ver.
-            services.AddSingleton<IProductService,ProductManager>();
-            services.AddSingleton<IProductDal,EfProductDal>();
+            //services.AddSingleton<IProductService,ProductManager>();
+            //services.AddSingleton<IProductDal,EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
